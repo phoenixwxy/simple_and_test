7a7e06a853aa17730dc85ccd30f3889a68be5f97
phoenix <wxymails@gmail.com>
2024-03-15 00:22:46 +0800

    initialize the project
    Signed-off-by: phoenix <wxymails@gmail.com>

---
+++ README.md
@@ -0,0 +1,15 @@
+# 简介
+## 目的
+这是一个用于在Android平台验证各种模块，功能的demo工程。
+
+全新的坑～～～
+
+## vcpkg支持
+vcpkg是由微软发起的，使用cmake组织的c++包管理器。
+
+[vcpkg cmake config file](./demo_vcpkg/src/main/cpp/cmake/vcpkg.cmake)
+
+**需要注意，使用时修改里面的代理设置和脚本**
+
+cmake和其他命令标准输出的位置
+`build/intermediates/cxx/Debug/l1h1x62h/logs/arm64-v8a/configure_stdout.txt`
---
+++ demo_vcpkg/src/main/cpp/cmake/vcpkg.cmake
@@ -0,0 +1,82 @@
+# local cache path
+set(PROJECT_CACHE_PATH ${CMAKE_CURRENT_SOURCE_DIR}/.cache)
+if (NOT EXISTS ${PROJECT_CACHE_PATH})
+    message("create build path:${PROJECT_CACHE_PATH}")
+    execute_process(COMMAND mkdir -p ${PROJECT_CACHE_PATH})
+endif ()
+
+# local not set ${VCPKG_ROOT} or no vcpkg
+if (NOT DEFINED ENV{VCPKG_ROOT})
+    if (NOT EXISTS ${PROJECT_CACHE_PATH}/vcpkg)
+        execute_process(COMMAND bash ${CMAKE_CURRENT_SOURCE_DIR}/scripts/proxy.sh
+                COMMAND git clone https://github.com/microsoft/vcpkg
+                WORKING_DIRECTORY ${PROJECT_CACHE_PATH}
+                COMMAND_ECHO STDOUT)
+    elseif (VCPKG_UPDATE)
+        execute_process(COMMAND bash ${CMAKE_CURRENT_SOURCE_DIR}/scripts/proxy.sh
+                COMMAND git pull .
+                WORKING_DIRECTORY ${PROJECT_CACHE_PATH}
+                COMMAND_ECHO STDOUT)
+    endif ()
+
+    if (VCPKG_UPDATE OR (NOT EXISTS ${PROJECT_CACHE_PATH}/vcpkg/vcpkg))
+        message("VCPKG bootstrap")
+        if (WIN32)
+            execute_process(COMMAND bootstrap-vcpkg.bat
+                    WORKING_DIRECTORY ${PROJECT_CACHE_PATH}/vcpkg)
+        else ()
+            execute_process(COMMAND bash bootstrap-vcpkg.sh
+                    WORKING_DIRECTORY ${PROJECT_CACHE_PATH}/vcpkg)
+        endif ()
+    endif ()
+
+    set(VCPKG_ROOT ${PROJECT_CACHE_PATH}/vcpkg)
+else ()
+    set(VCPKG_ROOT $ENV{VCPKG_ROOT})
+endif ()
+
+if (${CMAKE_SYSTEM_NAME} STREQUAL "Android")
+    message("This is Android platform")
+    set(VCPKG_TARGET_ANDROID ON)
+    if (DEFINED ANDROID_NDK)
+        set(ENV{ANDROID_NDK_HOME} ${ANDROID_NDK})
+    elseif (DEFINED CMAKE_ANDROID_NDK)
+        set(ENV{ANDROID_NDK_HOME} ${CMAKE_ANDROID_NDK})
+    else ()
+        message(FATAL_ERROR "
+                Please set an environment variable ANDROID_NDK_HOME
+                For example:
+                export ANDROID_NDK_HOME=/home/your-account/Android/Sdk/ndk-bundle
+                Or:
+                export ANDROID_NDK_HOME=/home/your-account/Android/android-ndk-r21b
+                ")
+    endif ()
+
+    if (ANDROID_ABI MATCHES "arm64-v8a")
+        set(VCPKG_TARGET_TRIPLET "arm64-android" CACHE STRING "" FORCE)
+    elseif (ANDROID_ABI MATCHES "armeabi-v7a")
+        set(VCPKG_TARGET_TRIPLET "arm-android" CACHE STRING "" FORCE)
+    elseif (ANDROID_ABI MATCHES "x86_64")
+        set(VCPKG_TARGET_TRIPLET "x64-android" CACHE STRING "" FORCE)
+    elseif (ANDROID_ABI MATCHES "x86")
+        set(VCPKG_TARGET_TRIPLET "x86-android" CACHE STRING "" FORCE)
+    else ()
+        message(FATAL_ERROR "
+                Please specify ANDROID_ABI
+                For example
+                cmake ... -DANDROID_ABI=armeabi-v7a
+
+                Possible ABIs are: arm64-v8a, armeabi-v7a, x64-android, x86-android
+                ")
+    endif ()
+
+    set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE $ENV{ANDROID_NDK_HOME}/build/cmake/android.toolchain.cmake)
+endif ()
+
+#    "--binarysource=default,readwrite"
+set(ENV{VCPKG_BINARY_SOURCES} "clear;files,${VCPKG_ROOT}/archives,readwrite;")
+set(VCPKG_INSTALL_OPTIONS "--allow-unsupported")
+set(CMAKE_TOOLCHAIN_FILE "${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake")
+
+message("CMAKE_TOOLCHAIN_FILE was set to ${CMAKE_TOOLCHAIN_FILE}")
+message("VCPKG_CHAINLOAD_TOOLCHAIN_FILE was set to ${VCPKG_CHAINLOAD_TOOLCHAIN_FILE}")
