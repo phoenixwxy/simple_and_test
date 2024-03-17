3e86f694025c8773eb4b19720d5b035080b90773
phoenix <wxymails@gmail.com>
2024-03-17 23:05:11 +0800

    更新module名字，添加单独vcpkg的READEME文件
    Signed-off-by: phoenix <wxymails@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.constraintlayout)
-    implementation(project(":demo_vcpkg"))
+    implementation(project(":vcpkg"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
