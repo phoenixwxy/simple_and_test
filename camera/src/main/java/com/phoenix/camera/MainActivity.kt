91e3febf5176eecbd21d43956f30bfed0bbe4846
Phoenix <wxymails@gmail.com>
2024-06-24 15:02:51 +0800

    [Feature][Camera] Init Module
    add a camera module for Camera
    
    Signed-off-by: Phoenix <wxymails@gmail.com>

---
+++ camera/src/main/java/com/phoenix/camera/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.phoenix.camera
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.phoenix.camera.ui.theme.Simple_testTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Simple_testTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Simple_testTheme {
+        Greeting("Android")
+    }
+}
